3d7db757f46f1f4ac567c49bd55f063c0b3a5b04
dirkluijk <mail@dirkluijk.nl>
2025-02-25 15:29:49 +0100

    chore: fix deno (2)

--- packages/core/eslint.config.mjs
+++ packages/core/eslint.config.mjs
@@ -1,11 +1,9 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import importPlugin from "eslint-plugin-import";
 
 export default tseslint.config(
+  // prettier-ignore
   eslint.configs.recommended,
-  importPlugin.flatConfigs.recommended,
-  importPlugin.flatConfigs.typescript,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
   {
@@ -16,14 +14,6 @@ export default tseslint.config(
     },
     rules: {
       "quote-props": ["error", "consistent-as-needed"],
-      "import/no-named-as-default-member": ["off"],
-      "import/order": [
-        "error",
-        {
-          "newlines-between": "always",
-        },
-      ],
-      "import/extensions": ["error", "always"],
     },
   },
 );
